78610ee85e582a6a95baaded0e5ff4ded10d6db0
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-07-15 01:46:34 +0300

    commit

--- src/components/TrainingList/TrainingItem/TrainingItem.tsx
+++ src/components/TrainingList/TrainingItem/TrainingItem.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import styles from "./training-item.module.css"
+import editIcon from "./icons/edit.png"
+import removeIcon from "./icons/remove.png"
 
 export type TrainingItemProps = {
-
+    date: string,
+    distance: number
 }
 
 export function TrainingItem(props: TrainingItemProps) {
+    const {date, distance} = props;
     return (
         <div className={styles.container}>
-
+            <span>{date}</span>
+            <span>{distance}</span>
+            <div>
+                <a href="#" className={styles.action}>
+                    <img src={editIcon} className={styles.icon}/>
+                </a>
+                <a href="#" className={styles.action}>
+                    <img src={removeIcon} className={styles.icon}/>
+                </a>
+            </div>
         </div>
     );
 }
--- src/components/TrainingList/TrainingList.tsx
+++ src/components/TrainingList/TrainingList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {TrainingAdd} from "./TrainingAdd";
 import styles from "./training-list.module.css"
+import {TrainingItem} from "./TrainingItem";
 
 export function TrainingList(props) {
     return (
@@ -11,6 +12,11 @@ export function TrainingList(props) {
                     <span>Дата (ДД.ММ.ГГ)</span>
                     <span>Пройдено км</span>
                     <span>Действия</span>
+                </div>
+                <div className={styles.content}>
+                    <TrainingItem date={"20.07.2019"} distance={5.7}></TrainingItem>
+                    <TrainingItem date={"19.07.2019"} distance={14.2}></TrainingItem>
+                    <TrainingItem date={"18.07.2019"} distance={3.4}></TrainingItem>
                 </div>
             </div>
         </>
